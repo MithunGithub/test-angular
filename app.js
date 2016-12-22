d203b385af8e5331b9d5a4b4e4ce085d6da83fe0
Mithun <mithun.jgec2008@gmail.com>
2016-12-23 02:30:31 +0530

    test file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-(function(){
-
-  angular.module('MyTestApp',[])
-  .controller('FirstController',function($scope){
-    $scope.name="Mithun";
-    $scope.test="love";
-    $scope.changeMe=function(){
-      $scope.test="like";
-    };
-  })
-})();
+// (function(){
+// 'use strict';
+//   angular.module('MyTestApp',[])
+//   .controller('FirstController',['$scope',function($scope){
+//     $scope.name="Mithun";
+//     $scope.test="love";
+//     $scope.changeMe=function(){
+//       $scope.test="like";
+//     };
+//   }])
+// })();
+!function(){"use strict";angular.module("MyTestApp",[]).controller("FirstController",["$scope",function(t){t.name="Mithun",t.test="love",t.changeMe=function(){t.test="like"}}])}();
